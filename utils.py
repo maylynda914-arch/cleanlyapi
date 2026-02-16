7fe63d2615f6d23985e621741b2f5681c1a841fc
maylynda914-arch <maylynda914@gmail.com>
2026-02-16 12:41:47 -0600

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+import json
+
+def load_users():
+    with open("users.json", "r") as f:
+        return json.load(f)
+
+def save_users(users):
+    with open("users.json", "w") as f:
+        json.dump(users, f, indent=4)
